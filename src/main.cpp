9cfcaa21322e9ef241ba2ca1b6d5de36d7ae98dd
Thursday the German <95447983+ThursdayTG@users.noreply.github.com>
2022-08-31 19:05:03 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -3,43 +3,38 @@
 #include <string>
 
 #include <chrono>
-#include <format>
+#include <fmt/core.h>
+#include <fmt/chrono.h>
 
 
 
 
-/*
 const std::string currentDateTime()
 {
-    time_t     now = time(0);
-    struct tm  tstruct;
-    char       buf[80];
-    tstruct = *localtime(&now);
-    strftime(buf, sizeof(buf), "%Y-%m-%d --- %X", &tstruct);
+    using namespace std::chrono;
+    time_point<system_clock> now = system_clock::now();
 
-    return buf;
+    return fmt::format("{0:%Y}-{0:%m}-{0:%d} --- {0:%X}", now);
 }
-*/
-
-
-
-
-const std::string currentDateTime() {
-    std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
-    return std::format("{%Y-%m-%d.%X}", now);
-}
-
-
 
 
 int main()
 {
-    std::ofstream outf{ currentDateTime() + " - new entry.md" };
+    using std::cout, std::endl;
+    std::string filename = currentDateTime() + " - new entry.md";
 
-    outf
-    << "Lorem ipsum dolor sit amet ...  " << "\n"
-    << "<br />" << "\n"
-    << "Lorem ipsum dolor sit amet ...  " << "\n";
+    {
+        std::ofstream outf{filename};
 
+        cout << "OFSTREAM OPENED, FILE GENERATED." << endl;
+
+        cout << "WRITING TO FILE." << endl;
+        outf
+        << "Lorem ipsum dolor sit amet ...  " << "\n"
+        << "<br />" << "\n"
+        << "Lorem ipsum dolor sit amet ...  " << "\n";
+    }
+
+    cout << "OFSTREAM CLOSED, CONTENT WRITTEN TO FILE \"" << filename << "\". EXITING." << endl;
     return 0;
 }
