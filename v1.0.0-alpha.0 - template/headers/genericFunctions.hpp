2d60039e8edd34d4596b29d71937c21a0e90faab
Thursday the German <95447983+ThursdayTG@users.noreply.github.com>
2022-08-31 18:47:03 +0200

    Add files via upload

---
+++ v1.0.0-alpha.0 - template/headers/genericFunctions.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+
+
+
+void clearScreen();
+
+void printHeader();
+
+bool queryRestart();
---
+++ v1.0.0-alpha.0 - template/src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+#include "../headers/genericFunctions.hpp"
+
+
+
+
+int main()
+{
+    using std::cout, std::cin;
+
+
+    bool   restartOperator = true;
+    while (restartOperator)
+    {
+        restartOperator = false;
+        printHeader();
+
+
+        //===== start ======
+        //lorem_ipsum
+
+
+        //=== operations ===
+        //lorem_ipsum
+
+
+        //====== end =======
+        restartOperator = queryRestart();
+        clearScreen();
+    }
+
+    return 0;
+}
