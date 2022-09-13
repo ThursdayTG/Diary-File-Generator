5a643ae19049a94de7df062a47677fd18861200d
Thursday the German <95447983+ThursdayTG@users.noreply.github.com>
2022-09-13 19:39:23 +0200

    Add files via upload

--- headers/currentDateTime.hpp
+++ headers/currentDateTime.hpp
@@ -6,17 +6,24 @@
 
 
 
-/*
+/**
  * To be completely honest, I (the author) do not fully understand this code to the extend I possible should,
  * but it does work entirely as I intend it to work.
  *
  * This function returns a string containing the current date and time in the following format:
- *     CCYY-MM-DD --- hh:mm:ss
+ *     CCYY-MM-DD ─ hh:mm:ss
+ *
+ * Example:
+ *     1970-12-24 ─ 23:59:59
+ *
+ *
  *
  * On Linux, the time is taken not directly from the system time, but rather from the time according to the
- * currently set timezone (usually, Linux-based OSs set system time to UTC regardless of timezone).
- * I have no idea how this will operate on Windows or Mac operating systems,
- * but it probably yields similar end results.
+ * currently set timezone (usually, Linux-based OSs set system time to UTC regardless of selected timezone).
+ * I have no idea how this will operate on Windows or Mac systems,
+ * but I assume it yields similar or identical end results.
+ *
+ * However, do note that not all file systems support all of these characters in filenames.
  */
 
 
